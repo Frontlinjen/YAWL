110907ddafa66639975d05c6a4a606ab97148d2f
Clusive <rdobrandelev@gmail.com>
2017-04-19 14:51:19 +0200

    Finished SelectedArcsHandler

--- dk.dtu.compute.mbse.yawl.simulator/src/application/SelectArcHandler.java
+++ dk.dtu.compute.mbse.yawl.simulator/src/application/SelectArcHandler.java
@@ -4,6 +4,13 @@ import org.eclipse.draw2d.MouseEvent;
 import org.pnml.tools.epnk.annotations.netannotations.ObjectAnnotation;
 import org.pnml.tools.epnk.applications.ui.IActionHandler;
 
+import dk.dtu.compute.mbse.yawl.TType;
+import dk.dtu.compute.mbse.yawl.Transition;
+import dk.dtu.compute.mbse.yawl.functions.YAWLFunctions;
+import yawlannotations.EnabledTransition;
+import yawlannotations.Marking;
+import yawlannotations.SelectedArc;
+
 public class SelectArcHandler implements IActionHandler{
 
 	private YAWLSimulator sim;
@@ -17,10 +24,62 @@ public class SelectArcHandler implements IActionHandler{
 	public boolean mouseDoubleClicked(MouseEvent arg0, ObjectAnnotation annotation) {
 		return false;
 	}
-
+	
 	@Override
 	public boolean mousePressed(MouseEvent arg0, ObjectAnnotation annotation) {
-		// TODO Auto-generated method stub
+		if(annotation instanceof SelectedArc){
+			SelectedArc selectedArc = (SelectedArc) annotation;
+			EnabledTransition targetTransition = (EnabledTransition) selectedArc.getTargetTransition();
+			EnabledTransition sourceTransition = (EnabledTransition) selectedArc.getSourceTransition();
+			Marking sourceMarking = selectedArc.getSourceMarking();
+			
+			if(targetTransition != null){
+				if(!selectedArc.isSelected() &&
+					sourceMarking != null &&
+					sourceMarking.getValue() > 0){
+				Transition transition = (Transition) ((yawlannotations.EnabledTransition) targetTransition).getTransition();
+				if(YAWLFunctions.getJoinType(transition).equals(TType.XOR)){
+					for(SelectedArc s_arc: targetTransition.getInArcs()){
+						s_arc.setSelected(false);
+					}
+					selectedArc.setSelected(true);
+					sim.update();
+					return true;
+				}
+				}else if(sourceTransition != null){
+					Transition transition = (Transition) ((yawlannotations.EnabledTransition) sourceTransition).getTransition();
+					TType cType = YAWLFunctions.getSplitType(transition);
+					if(cType.equals(TType.XOR)){
+						for(SelectedArc s_arc: sourceTransition.getOutArcs()){
+							s_arc.setSelected(false);
+						}
+						selectedArc.setSelected(true);
+						sim.update();
+						return true;
+				}else if(cType.equals(TType.OR)){
+					selectedArc.setSelected(!selectedArc.isSelected());
+					boolean notEmpty = false;
+					for (SelectedArc s_arc: sourceTransition.getOutArcs()){
+						if(s_arc.isSelected()){
+							notEmpty = true;
+							break;
+						}
+					}
+					if(!notEmpty){
+						for (SelectedArc s_arc: sourceTransition.getOutArcs()){
+							if(s_arc != selectedArc){
+								s_arc.setSelected(true);
+								break;
+							}
+						}
+					}
+					sim.update();
+					return true;
+				}
+					
+				}
+			}
+		}
 		return false;
 	}
 
