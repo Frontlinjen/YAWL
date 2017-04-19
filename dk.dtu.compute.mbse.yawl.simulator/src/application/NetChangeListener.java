50954e6a619ee0628ed41487b699fdc6bd33133b
MikkelLeth <mkleth10@gmail.com>
2017-04-19 12:32:21 +0200

    Started to make some of the functions from tutorial 8.

--- dk.dtu.compute.mbse.yawl.simulator/src/application/NetChangeListener.java
+++ dk.dtu.compute.mbse.yawl.simulator/src/application/NetChangeListener.java
@@ -3,6 +3,9 @@ package application;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.pnml.tools.epnk.helpers.FlatAccess;
 
 public class NetChangeListener implements Adapter{
 	
@@ -15,13 +18,34 @@ public class NetChangeListener implements Adapter{
 
 	@Override
 	public void notifyChanged(Notification notification) {
-		// TODO Auto-generated method stub
+		String name = sim.getName();
+		if(name != null){
+			name = "Technical simulator: " + name;
+		} else{
+			name = "Technical simulator";
+		}
 		
+		FlatAccess flatAccess = sim.flatAccess;
+		if(flatAccess != null){
+			flatAccess.removeInvalidationListener(this);
+			sim.flatAccess = null;
+		}
+		
+		final String label = name;
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run(){
+				if (MessageDialog.openConfirm(null, label, 
+						"The underlying net has been modified." + System.lineSeparator() +
+						"It is unsafe to continue using the application, and it may show wrong results." + System.lineSeparator() +
+						"Do you want stop this application (recommended)?")) {
+					sim.dispose();
+				}
+			}
+		});
 	}
 
 	@Override
 	public Notifier getTarget() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -33,7 +57,6 @@ public class NetChangeListener implements Adapter{
 
 	@Override
 	public boolean isAdapterForType(Object type) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
