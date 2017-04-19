488ecf4c1eaba61752a6544bae3e78280fcbbd9f
Hjorthen <Hjorthensebastian@gmail.com>
2017-04-19 11:02:03 +0200

    Implemented NetMarking

--- dk.dtu.compute.mbse.yawl.simulator/src/marking/NetMarking.java
+++ dk.dtu.compute.mbse.yawl.simulator/src/marking/NetMarking.java
@@ -1,9 +1,13 @@
 package marking;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.pnml.tools.epnk.pnmlcoremodel.Place;
+
+import dk.dtu.compute.mbse.tutorial.yawl.simulator.marking.Condition;
 
 public class NetMarking {
 	
@@ -11,6 +15,80 @@ public class NetMarking {
 	
 	public NetMarking(){
 		marking = new HashMap<Place,Integer>();
+	}
+	
+	public void SetMarking(Place p, int amount)
+	{
+		if(amount > 0)
+			marking.put(p, amount);
+		else 
+			marking.remove(p);
+	}
+	
+	public int GetMarking(Place p)
+	{
+		return marking.getOrDefault(p, 0);
+	}
+	
+	public void Increment(Place p, int amount){
+		SetMarking(p, GetMarking(p) + amount);
+	}
+	
+	public void Decrement(Place p, int amount){
+		SetMarking(p, GetMarking(p) - amount);
+	}
+	
+	public void Add(NetMarking m){
+		Iterator it = m.marking.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry pair = (Map.Entry)it.next();
+			int val1 = marking.getOrDefault(pair.getKey(), 0);
+			int val2 = (int) pair.getValue();
+			if(val1 + val2 > 0)
+			{
+				marking.put((Place)pair.getKey(), val1 + val2);
+			}
+			else
+			{
+				marking.remove(pair.getKey());
+			}
+		}
+	}
+	
+	public void Subtract(NetMarking m)
+	{
+		Iterator it = m.marking.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry pair = (Map.Entry)it.next();
+			int val1 = marking.getOrDefault(pair.getKey(), 0);
+			int val2 = (int) pair.getValue();
+			if(val1 - val2 > 0)
+			{
+				marking.put((Place)pair.getKey(), val1 - val2);
+			}
+			else
+			{
+				marking.remove(pair.getKey());
+			}
+		}
+	}
+	
+	public boolean GreaterOrEqual(NetMarking m)
+	{
+		Iterator it = m.marking.entrySet().iterator();
+		while(it.hasNext()){
+			Map.Entry pair = (Map.Entry)it.next();
+			int val1 = marking.getOrDefault(pair.getKey(), 0);
+			int val2 = (int) pair.getValue();
+			if(val1 < val2){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public Collection<Place> GetSupport() {
+		return java.util.Collections.unmodifiableCollection(marking.keySet());
 	}
 	
 }
